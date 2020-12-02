195ba283f0d22d4472c0f78fb32e504d39afe300
BoriSantiago <boris96bs7@gmail.com>
2020-12-02 18:27:17 -0500

    Arreglo de post

--- TiendaBackend/src/main/java/net/e3/Tuiendaprueba/controller/CategoriaController.java
+++ TiendaBackend/src/main/java/net/e3/Tuiendaprueba/controller/CategoriaController.java
@@ -1,13 +1,4 @@
-																/*
- -------------------------------------------------------------------
-|
-| CRUDyLeaf	- A Domain Specific Language for generating Spring Boot 
-|			REST resources from entity CRUD operations.
-| Author: Omar S. G�mez (2020)
-| File Date: Sun Aug 09 00:26:19 COT 2020
-| 
- -------------------------------------------------------------------
-																*/
+
 package net.e3.Tuiendaprueba.controller;
 
 import net.e3.Tuiendaprueba.entity.Categoria;
@@ -70,8 +61,6 @@ public class CategoriaController {
 			return new ResponseEntity<Categoria>(HttpStatus.CONFLICT); 
 		}
 			
-		
-		
 	}
 
 	@PutMapping("/categoria")
